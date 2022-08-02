d14060130cc861f60ecd489b680d5a00f74f5f36
Ivuska <fischerova.ivka@gmail.com>
2022-08-02 08:11:54 +0200

    Added pagination.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,6 +3,25 @@
   <div class="events">
     <h1>Events for Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous
+      </router-link>
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next &#62;
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -10,29 +29,46 @@
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService.js";
+import { watchEffect } from "vue";
 
 export default {
   name: "EventList",
+  props: ["page"],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     };
   },
   //Lifecycle hook (component have more lifecycle hooks), when the component is created it calls the server for the events stored in the
   //mocked db on the URL below.
   //Axios (stored in EventService) is asynced and promise based so we need to wait for the request. Achieve that we use then().
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data;
-      })
-      //If the request is rejected catch the situation.
-      .catch((error) => {
-        console.log(error);
-      });
+    watchEffect(() => {
+      // This clear out the events on the page, so user knows that API has been called.
+      this.event = null;
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data;
+          this.totalEvents = response.headers["x-total-count"];
+        })
+        // If the request is rejected catch the situation.
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  },
+  computed: {
+    hasNextPage() {
+      // Finds the total number of pages.
+      var totalPages = Math.ceil(this.totalEvents / 2);
+
+      // If the page is not the last page show 'Next Page' link in Pagination.
+      return this.page < totalPages;
+    },
   },
 };
 </script>
@@ -42,5 +78,22 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
